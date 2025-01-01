0f3229ef068d27eb1e2f21fa08ad0fb7ee7d66b5
chanuka1012 <chanukarathnayake1012@gmail.com>
2025-01-01 12:14:06 +0530

    basic

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ReactDOM from "react-dom/client";
+import HomePage from './Components/HomePage/HomePage';
+import Register from './Components/Register/Register';
+import Login from './Components/Login/Login';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,10 +15,11 @@ function App() {
     <>
       <BrowserRouter>
        <Routes>
-        <Route path="/" element={<Home/>} />
+         <Route path="/" element={<HomePage />} />
+         <Route path="/register" element={<Register />} />
+         <Route path="/login" element={<Login />} />
        </Routes>
-      </BrowserRouter>
-      
+      </BrowserRouter> 
     </>
   );
 }
